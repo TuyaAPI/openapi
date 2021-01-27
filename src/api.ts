dc31f32b135adc9739457e3690e512f8f332f106
Milo Cesar <milocesar1@gmail.com>
2021-01-27 20:31:14 +0100

    Extended API typings
    Changed the default `object` typings to more specific typings.
    Fixed some API endpoints that were wrongly typed as `Promise<string>`,
    which should also have been of the `object` type.

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,17 @@
 import * as crypto from 'crypto';
 import got, {Got, Response} from 'got';
+import {Device} from './device';
+import {
+  DevicesResponse,
+  DeviceTokenResponse,
+  PairingResultResponse,
+  TokenResponse,
+  TuyaResponse,
+  UsersResponse
+} from './responses';
 
-interface TuyaResponse {
-  msg: string;
-  t: number;
-  success: boolean;
-  result: object;
-}
+// Re-export common types for further manipulation by consumers
+export {Device, Status} from './device';
 
 export class OpenAPI {
   public tokenAccess: string;
@@ -49,10 +54,10 @@ export class OpenAPI {
               await this.refreshToken();
             }
 
-            const now = new Date().getTime();
+            const now = Date.now();
             options.headers.t = now.toString();
 
-            // Caculate signature
+            // Calculate signature
             let sign = '';
 
             if (isTokenUrl) {
@@ -74,13 +79,13 @@ export class OpenAPI {
         ],
         afterResponse: [
           (response: Response) => {
-            const body = response.body as TuyaResponse;
+            const body = response.body as TuyaResponse<unknown>;
 
             if (!body.success) {
               throw new Error(body.msg);
             }
 
-            response.body = body.result as TuyaResponse;
+            response.body = body;
 
             return response;
           }
@@ -91,7 +96,7 @@ export class OpenAPI {
 
   // Authorization methods
   isTokenExpired(): boolean {
-    return new Date().getTime() > this.tokenExpiresAt.getTime();
+    return Date.now() > this.tokenExpiresAt.getTime();
   }
 
   async getToken(): Promise<void> {
@@ -99,24 +104,24 @@ export class OpenAPI {
       throw new HandleTokenError();
     }
 
-    const {body: {access_token, refresh_token, expire_time}} = await this._client.get('token?grant_type=1');
+    const {body: {access_token, refresh_token, expire_time}} = await this._client.get<TokenResponse>('token?grant_type=1');
 
     this.tokenAccess = access_token;
     this.tokenRefresh = refresh_token;
-    this.tokenExpiresAt = new Date(new Date().getTime() + (expire_time * 1000));
+    this.tokenExpiresAt = new Date(Date.now() + (expire_time * 1000));
   }
 
   async refreshToken(): Promise<void> {
-    const {body: {access_token, refresh_token, expire_time}} = await this._client.get(`token/${this.tokenRefresh}`);
+    const {body: {access_token, refresh_token, expire_time}} = await this._client.get<TokenResponse>(`token/${this.tokenRefresh}`);
 
     this.tokenAccess = access_token;
     this.tokenRefresh = refresh_token;
-    this.tokenExpiresAt = new Date(new Date().getTime() + (expire_time * 1000));
+    this.tokenExpiresAt = new Date(Date.now() + (expire_time * 1000));
   }
 
   // API methods
   async putUser({countryCode, username, password, usernameType, nickname}: {countryCode: string; username: string; password: string; usernameType: string; nickname: string}): Promise<string> {
-    const req = {
+    const request = {
       schema: this.schema,
       country_code: countryCode,
       username,
@@ -125,79 +130,76 @@ export class OpenAPI {
       nick_name: nickname
     };
 
-    const res = await this._client.post(`apps/${this.schema}/user`, {
-      json: req
-    });
-
     interface UserResponse {
       uid: string;
     }
 
-    const {uid}: {uid: string} = res.body as unknown as UserResponse;
+    const response = await this._client.post<UserResponse>(`apps/${this.schema}/user`, {
+      json: request
+    });
 
-    return uid;
+    return response.body.uid;
   }
 
-  async getUsers({pageNumber, pageSize}: {pageNumber: number; pageSize: number} = {pageNumber: 1, pageSize: 100}): Promise<object> {
-    const res = await this._client.get(`apps/${this.schema}/users`, {
+  async getUsers({pageNumber, pageSize}: {pageNumber: number; pageSize: number} = {pageNumber: 1, pageSize: 100}): Promise<UsersResponse> {
+    const response = await this._client.get<UsersResponse>(`apps/${this.schema}/users`, {
       searchParams: {
         page_no: pageNumber,
         page_size: pageSize
       }
     });
-
-    return res.body as unknown as object;
+    return response.body;
   }
 
-  async getDeviceToken({uid, timezone}: {uid: string; timezone: string}): Promise<string> {
-    const res = await this._client.post('devices/token', {
+  async getDeviceToken({uid, timezone}: {uid: string; timezone: string}): Promise<DeviceTokenResponse> {
+    const response = await this._client.post<DeviceTokenResponse>('devices/token', {
       json: {
         uid,
         timeZoneId: timezone
       }
     });
 
-    return res.body;
+    return response.body;
   }
 
-  async getDevicesByToken(token: string): Promise<object> {
-    const res = await this._client.get(`devices/tokens/${token}`);
+  async getDevicesByToken(token: string): Promise<PairingResultResponse> {
+    const response = await this._client.get<PairingResultResponse>(`devices/tokens/${token}`);
 
-    return res.body as unknown as object;
+    return response.body;
   }
 
-  async getDevicesByUser(uid: string): Promise<object> {
-    const res = await this._client.get(`users/${uid}/devices`);
+  async getDevicesByUser(uid: string): Promise<Device[]> {
+    const response = await this._client.get<Device[]>(`users/${uid}/devices`);
 
-    return res.body as unknown as object;
+    return response.body;
   }
 
-  async getDevices({ids, pageNumber = 0, pageSize = 100}: {ids?: string[]; pageNumber: number; pageSize: number} = {pageNumber: 0, pageSize: 100}): Promise<object> {
-    const searchParams: any = {
+  async getDevices({ids, pageNumber = 0, pageSize = 100}: {ids?: string[]; pageNumber: number; pageSize: number} = {pageNumber: 0, pageSize: 100}): Promise<DevicesResponse> {
+    const searchParameters: any = {
       schema: this.schema,
       page_no: pageNumber,
       page_size: pageSize
     };
 
     if (ids) {
-      searchParams.device_ids = ids.toString();
+      searchParameters.device_ids = ids.toString();
     }
 
-    const res = await this._client.get('devices', {searchParams});
+    const response = await this._client.get<DevicesResponse>('devices', {searchParams: searchParameters});
 
-    return res.body as unknown as object;
+    return response.body;
   }
 
-  async getDevice(deviceId: string): Promise<object> {
-    const res = await this._client.get(`devices/${deviceId}`);
+  async getDevice(deviceId: string): Promise<Device> {
+    const response = await this._client.get<Device>(`devices/${deviceId}`);
 
-    return res.body as unknown as object;
+    return response.body;
   }
 
-  async getDeviceStatus(deviceId: string): Promise<object> {
-    const res = await this._client.get(`devices/${deviceId}/status`);
+  async getDeviceStatus(deviceId: string): Promise<Device['status']> {
+    const response = await this._client.get<Device['status']>(`devices/${deviceId}/status`);
 
-    return res.body as unknown as object;
+    return response.body;
   }
 }
 
---
+++ src/responses.ts
@@ -0,0 +1,54 @@
+import {Device} from './device';
+
+export interface TuyaResponse<T extends object | unknown> {
+  msg: string;
+  t: number;
+  success: boolean;
+  result: T;
+}
+
+export interface TokenResponse {
+  access_token: string;
+  refresh_token: string;
+  expire_time: number;
+}
+
+export interface UsersResponse {
+  has_mode: boolean;
+  list: Array<{
+    create_time: number;
+    email: string;
+    uid: string;
+    update_time: number;
+    username: string;
+  }>;
+  total: number;
+}
+
+export interface DeviceTokenResponse {
+  expire_time: number;
+  region: string;
+  secret: string;
+  token: string;
+}
+
+export interface PairingResultResponse {
+  errorDevices: Array<{
+    device_id: string;
+    product_id: string;
+    name: string;
+    category: string;
+  }>;
+  successDevices: Array<{
+    device_id: string;
+    code: string;
+    msg: string;
+    name: string;
+  }>;
+}
+
+export interface DevicesResponse {
+  total: number;
+  devices: Device[];
+  last_id?: string;
+}
